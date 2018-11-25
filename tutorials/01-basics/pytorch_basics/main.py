1c31cd054eeb21ead07a80d9b19f68f9842b68ec
lixiao1009 <743266110@qq.com>
2018-11-25 18:27:15 +0800

    Update main.py

--- tutorials/01-basics/pytorch_basics/main.py
+++ tutorials/01-basics/pytorch_basics/main.py
@@ -3,6 +3,8 @@ import torchvision
 import torch.nn as nn
 import numpy as np
 import torchvision.transforms as transforms
+import torch.nn.functional as F
+
 
 
 # ================================================================== #
